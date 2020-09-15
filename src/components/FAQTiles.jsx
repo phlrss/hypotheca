2015fe6d97d72bfec03f0811a61bffab682e86bf
Philip Russo <philip.russo@onespan.com>
2020-09-14 20:19:05 -0400

    minor fixes

--- src/components/FAQTiles.jsx
+++ src/components/FAQTiles.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom'
 
 export const FAQTiles = ({ setFaqPosition }) => {
   return (
-    <div className="w-full flex flex-col justify-center p-12 bg-gray-500 bg-opacity-25">
+    <div className="w-full flex flex-col justify-center p-8 lg:p-12 xl:p-24 bg-gray-500 bg-opacity-25">
       <div className="flex flex-col lg:flex-row justify-center">
         { faqs.filter((e, index) => index < 3).map((faq, index) => (
           <Link onClick={() => {setFaqPosition(index)}} to="/faq" key={index} className="text-left transition-all duration-300 hover:bg-gray-200 hover:cursor-pointer p-5 mb-5 mr-4 bg-white rounded flex items-center shadow-md w-full lg:max-w-sm">
-            <label className="block text-gray-700 font-bold mb-2">
+            <span className="block text-gray-700 font-bold mb-2 flex-grow">
               {faq.question}
-            </label>
+            </span>
           </Link>
         ))}
       </div>
       <div className="flex flex-col lg:flex-row justify-center">
         { faqs.filter((e, index) => index > 2 && index < 6).map((faq, index) => (
           <Link onClick={() => {setFaqPosition(index + 3)}} to="/faq" key={index} className="text-left transition-all duration-300 hover:bg-gray-200 hover:cursor-pointer p-5 mb-5 mr-4 bg-white rounded flex items-center shadow-md w-full lg:max-w-sm">
-            <label className="block text-gray-700 font-bold mb-2">
+            <span className="block text-gray-700 font-bold mb-2">
               {faq.question}
-            </label>
+            </span>
           </Link>
         ))}
       </div>
